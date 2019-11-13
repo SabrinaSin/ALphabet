c0d1a126536138ee8427f3bc055d442bda909e69
Sabrina Singh <3155488@student.ocadu.ca>
2019-11-13 17:48:10 -0500

    More Changes

--- Space/js/SPACE.js
+++ Space/js/SPACE.js
@@ -1,16 +1,87 @@
 
-$(document).scroll(function() {
+$(".circle").scrollTo(function(){
 
-	var scrolledSoFar = $(document).scrollTop()
-	var totalHeight = $(document).height()-$(window).height();
-	var scrollPercentage = scrolledSoFar / totalHeight;
-	var scrollDegree = scrollPercentage * 360
+	var controller = new ScrollMagic.Controller();
 
-		$(".MOON").css({
+	var tween = TweenMax.to("animate", 1, {scale: 3, ease: Linear.easeNone});
 
-				"transform":"rotate("+scrollDegree+"deg)"
-		})
+	var scene = new ScrollMagic.Scene({triggerElement: "#multiDirect", duration: 400, offset: 250})
+						.setTween(tween)
+						.setPin("#animate")
+						.addIndicators({name: "resize"}) // add indicators (requires plugin)
+						.addTo(controller);
 
-		console.log(scrollDegree)
+		var controller_h = new ScrollMagic.Controller({vertical: false});
 
-})
+		var tween_h = TweenMax.to("#animate", 1, {ease: Linear.easeNone});
+
+		var scene_h = new ScrollMagic.Scene({duration: 700})
+						.setTween(tween_h)
+						.setPin("#animate")
+						.addTo(controller_h);
+	});
+
+
+
+
+
+
+
+
+
+//$(function () { // wait for document ready
+//		// init controller
+//		var controller = new ScrollMagic.Controller();
+//
+//		// build tween
+//		var tween = TweenMax.to("#animate", 0.5, {scale: 3, ease: Linear.easeNone});
+//
+//		// build scene
+//		var scene = new ScrollMagic.Scene({triggerElement: "#multiDirect", duration: 400, offset: 250})
+//						.setTween(tween)
+//						.setPin("#animate")
+//						.addIndicators({name: "resize"}) // add indicators (requires plugin)
+//						.addTo(controller);
+//
+//		// init controller horizontal
+//		var controller_h = new ScrollMagic.Controller({vertical: false});
+//
+//		// build tween horizontal
+//		var tween_h = TweenMax.to("#animate", 0.5, {rotation: 360, ease: Linear.easeNone});
+//
+//		// build scene
+//		var scene_h = new ScrollMagic.Scene({duration: 700})
+//						.setTween(tween_h)
+//						.setPin("#animate")
+//						.addIndicators({name: "rotate"}) // add indicators (requires plugin)
+//						.addTo(controller_h);
+//	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// $(document).scroll(function() {
+
+//	var scrolledSoFar = $(document).scrollTop()
+//	var totalHeight = $(document).height()-$(window).height();
+//	var scrollPercentage = scrolledSoFar / totalHeight;
+//	var scrollDegree = scrollPercentage * 360
+
+//		$(".MOON").css({
+
+//				"transform":"rotate("+scrollDegree+"deg)"
+//		})
+
+//		console.log(scrollDegree)
+
+//})
